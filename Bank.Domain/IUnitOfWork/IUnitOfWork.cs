805cb45db48680cce3b2388b336b0e4d281e1466
omar_Ashraf <omar_Ashraf@OMAR>
2026-02-28 02:35:08 +0200

    Contexts

--- Bank.Domain/IUnitOfWork/IUnitOfWork.cs
+++ Bank.Domain/IUnitOfWork/IUnitOfWork.cs
@@ -13,26 +13,6 @@ namespace Bank.Domain.IUnitOfWork
         public Task<int> SaveChangesAsync();
         public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : class;
     }
-    public class UnitOfWork : IUnitOfWork
-    {
-        private readonly BankDbContext _dbContext;
-        private readonly Dictionary<Type, object> _repositories = [];
-        public UnitOfWork(BankDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : class
-        {
-            var EntityType = typeof(TEntity);
-            if (_repositories.TryGetValue(EntityType, out object? repository))
-                return (IGenericRepository<TEntity, TKey>)repository;
-            var newRepository = new GenericRepository<TEntity, TKey>(_dbContext);
-            _repositories[EntityType] = newRepository;
-            return newRepository;
-        }
 
-        public async Task<int> SaveChangesAsync()
-         => await _dbContext.SaveChangesAsync();
-    }
     #endregion
 }
--- Bank.Persistence/DbContexts/BankDbContext.cs
+++ Bank.Persistence/DbContexts/BankDbContext.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bank.Domain.Entities.AccountModule;
+using Bank.Domain.Entities.IdentityModule;
 using Bank.Domain.Entities.TransactionModule;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bank.Persistence.DbContexts
 {
+    #region Contexts
     public class BankDbContext : DbContext
     {
         public BankDbContext(DbContextOptions<BankDbContext> options) : base(options) { }
@@ -19,4 +23,22 @@ namespace Bank.Persistence.DbContexts
         public DbSet<BankAccount> BankAccounts { get; set; }
         public DbSet<Transaction> transactions { get; set; }
     }
+    public class BankIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BankIdentityDbContext(DbContextOptions<BankIdentityDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Address>().ToTable("Addresses");
+            builder.Entity<ApplicationUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<ApplicationUser>()
+                   .HasOne(a => a.Address)
+                   .WithOne(a => a.User)
+                   .HasForeignKey<Address>(a => a.UserId);
+        }
+    }
+    #endregion
 }
